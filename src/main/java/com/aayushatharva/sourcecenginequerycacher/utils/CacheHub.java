f477ae8497ac7defd02e5d4a70b124f9ec8734be
Aayush Atharva <aayush@shieldblaze.com>
2020-07-30 20:30:45 +0530

    Remove `expireAfterAccess`

--- src/main/java/com/aayushatharva/sourcecenginequerycacher/utils/CacheHub.java
+++ src/main/java/com/aayushatharva/sourcecenginequerycacher/utils/CacheHub.java
@@ -24,7 +24,6 @@ public final class CacheHub {
      */
     public static final Cache<String, String> CHALLENGE_CACHE = CacheBuilder.newBuilder()
             .maximumSize(Config.MaxChallengeCode)
-            .expireAfterAccess(Duration.ofMillis(Config.ChallengeCodeTTL))
             .expireAfterWrite(Duration.ofMillis(Config.ChallengeCodeTTL))
             .concurrencyLevel(Config.ChallengeCodeCacheConcurrency)
             .build();
