a1a3ad3dbba51e3b9d8227928e9eef634fe5d830
Michael Phoenix <3187643-Michael-Phoenix@users.noreply.gitlab.com>
2021-11-11 11:37:38 +0100

    Undo AdaptiveRecvByteBufAllocator change for Player

--- src/main/java/com/aayushatharva/sourcecenginequerycacher/gameserver/a2splayer/PlayerClient.java
+++ src/main/java/com/aayushatharva/sourcecenginequerycacher/gameserver/a2splayer/PlayerClient.java
@@ -29,7 +29,7 @@ public final class PlayerClient extends Thread {
                     .option(ChannelOption.ALLOCATOR, Main.BYTE_BUF_ALLOCATOR)
                     .option(ChannelOption.SO_SNDBUF, Config.SendBufferSize)
                     .option(ChannelOption.SO_RCVBUF, Config.ReceiveBufferSize)
-                    .option(ChannelOption.RCVBUF_ALLOCATOR, new AdaptiveRecvByteBufAllocator(Config.ReceiveAllocatorBufferSizeMin, Config.ReceiveAllocatorBufferSize, Config.ReceiveAllocatorBufferSizeMax))
+                    .option(ChannelOption.RCVBUF_ALLOCATOR, new AdaptiveRecvByteBufAllocator())
                     .handler(new PlayerHandler());
 
             Channel channel = bootstrap.connect(Config.GameServer).sync().channel();
