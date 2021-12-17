691b32309bfbc1e7d895e318df89e0acb53a96d0
Aayush Atharva <aayush@shieldblaze.com>
2021-12-17 13:26:07 +0530

    finish

--- src/main/java/com/aayushatharva/seqc/Main.java
+++ src/main/java/com/aayushatharva/seqc/Main.java
@@ -81,10 +81,10 @@ public final class Main {
                     .option(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT)
                     .option(ChannelOption.SO_SNDBUF, Config.SendBufferSize)
                     .option(ChannelOption.SO_RCVBUF, Config.ReceiveBufferSize)
-//                    .option(EpollChannelOption.MAX_DATAGRAM_PAYLOAD_SIZE, Config.ReceiveAllocatorBufferSize)
-//                    .option(ChannelOption.RCVBUF_ALLOCATOR, new FixedRecvByteBufAllocator(Config.ReceiveAllocatorBufferSize))
+                    .option(EpollChannelOption.MAX_DATAGRAM_PAYLOAD_SIZE, Config.ReceiveAllocatorBufferSize)
+                    .option(ChannelOption.RCVBUF_ALLOCATOR, new FixedRecvByteBufAllocator(Config.ReceiveAllocatorBufferSize))
                     .option(UnixChannelOption.SO_REUSEPORT, true)
-                    .option(EpollChannelOption.UDP_GRO, true) // Enable UDP GRO
+                    .option(EpollChannelOption.UDP_GRO, true)
                     .handler(handler);
 
             for (int i = 0; i < Config.Threads; i++) {
